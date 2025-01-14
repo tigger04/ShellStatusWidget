bb29ecea6077191608efad590c2c9661e8505972
tigger <tigger04@gmail.com>
2025-01-14 11:06:40 +0000

    AMAHKWPXXGDMC:acp

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+from objc import objc_object
+from AppKit import NSApplication, NSWindowBelow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My PyQt App")
+        self.setWindowTitle("Desktop-Level Window")
         self.setGeometry(100, 100, 400, 200)
 
         # Add a label
-        label = QLabel("Hello, PyQt!", self)
+        label = QLabel("This is a desktop-level window.", self)
         label.setAlignment(Qt.AlignCenter)
 
         # Set the central widget
@@ -18,6 +21,23 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+
+        # Make the window stay behind all others
+        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
+
+        # Set desktop level on macOS
+        if sys.platform == "darwin":
+            self.set_mac_desktop_level()
+
+    def set_mac_desktop_level(self):
+        from ctypes import cdll, c_void_p
+
+        # Access the native macOS window
+        window_handle = self.winId()
+        appkit = cdll.LoadLibrary("/System/Library/Frameworks/AppKit.framework/AppKit")
+        ns_window = objc_object(c_void_p=window_handle)
+        ns_window.setLevel_(NSWindowBelow)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
